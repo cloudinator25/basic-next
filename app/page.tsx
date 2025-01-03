79c5fda5f913f3631aa13ef1e4297b114c8097a1
Vathanak <102912156+VathanakSol@users.noreply.github.com>
2025-01-03 19:21:22 +0000

    change version

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@
 export default function Home() {
   return (
     <div className="w-full h-screen grid place-content-center bg-purple-500 hover:bg-purple-700 text-white text-3xl transition-all ease-in-out">
-      <h1>This is a another new version (3)</h1>
+      <h1>This is a another new version (4)</h1>
     </div>
   );
 }
