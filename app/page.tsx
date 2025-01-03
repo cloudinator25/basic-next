47ecf403f78bc44f0f56a7f363f6e93cbdc5cf03
Vathanak <102912156+VathanakSol@users.noreply.github.com>
2025-01-03 18:24:38 +0000

    update new code

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,8 @@
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <h1>This is a New Version 2</h1>
+    <div className="w-full h-screen grid place-content-center bg-purple-500 hover:bg-purple-700 text-white text-3xl transition-all ease-in-out">
+      <h1>This is a another new version (3)</h1>
     </div>
   );
 }
