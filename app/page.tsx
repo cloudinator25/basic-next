f047d26846717f885132845811ed160d8b5457d5
VathanakSol <vathanaksol1605@gmail.com>
2025-01-06 09:04:17 +0700

    modified file

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,16 @@
-
 export default function Home() {
   return (
-    <div className="w-full h-screen grid place-content-center bg-purple-500 hover:bg-purple-700 text-white text-3xl transition-all ease-in-out">
-      <h1>Version 5</h1>
+    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+      <h1 className="text-6xl font-bold">
+        Welcome to <a href="https://nextjs.org">Next.js!</a>
+      </h1>
+      <p className="mt-3 text-2xl">
+        Get started by editing&nbsp;
+        <code className="p-1 font-mono text-sm rounded-md bg-gray-100">
+          pages/index.tsx
+        </code>
+      </p>
     </div>
   );
 }
+
