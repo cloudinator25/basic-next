d9183be8daf09e07a08c805d2d3be7092f287ab8
VathanakSol <vathanaksol1605@gmail.com>
2025-01-06 09:12:01 +0700

    change

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ export default function Home() {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen py-2">
       <h1 className="text-6xl font-bold">
-        Welcome to <a href="https://nextjs.org">Next.js!</a>
+        Welcome to <a href="https://nextjs.org">Deploy Next.js!</a>
       </h1>
       <p className="mt-3 text-2xl">
         Get started by editing&nbsp;
